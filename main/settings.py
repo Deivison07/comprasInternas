fb8a5d3ccb15124d200d0c90e6fa94cb9bf5b17e
Deivison de alcantara santos <Deivisonalc02@gmail.com>
2021-10-13 02:26:30 -0300

    adicionando imagens(conclusao do projeto)

--- main/settings.py
+++ main/settings.py
@@ -77,9 +77,19 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'main',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
+#
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+
 }
 
 
